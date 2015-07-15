0ad1edd481716d8c971b36956e14539c6f9bc2f0
Evan You <yyx990803@gmail.com>
2015-07-15 06:39:40 -0700

    conditionally show stuff

--- example/src/components/app.vue
+++ example/src/components/app.vue
@@ -22,6 +22,9 @@ export default {
     },
     filteredTodos () {
       return filters[this.filter](this.todos)
+    },
+    remaining () {
+      return this.todos.filter(todo => !todo.done).length
     }
   },
   methods: {
@@ -40,19 +43,23 @@ export default {
   <div>
     <input type="checkbox"
       checked="{{allChecked}}"
+      v-show="todos.length"
       v-on="change: TOGGLE_ALL_TODOS(!allChecked)">
     <input v-on="keyup: addTodo | key 'enter'">
     <ul class="todo-list">
       <todo v-repeat="todo: filteredTodos"></todo>
     </ul>
-    <p>
-      <a v-class="active: filter==='all'"
-        v-on="click: SET_FILTER('all')">all</a>
-      <a v-class="active: filter==='done'"
-        v-on="click: SET_FILTER('done')">done</a>
-      <a v-class="active: filter==='notDone'"
-        v-on="click: SET_FILTER('notDone')">not done</a>
-    </p>
-    <button v-on="click: CLEAR_DONE_TODOS">Clear Done</button>
+    <div class="footer" v-show="todos.length">
+      <p>
+        <a v-class="active: filter==='all'"
+          v-on="click: SET_FILTER('all')">all</a>
+        <a v-class="active: filter==='done'"
+          v-on="click: SET_FILTER('done')">done</a>
+        <a v-class="active: filter==='notDone'"
+          v-on="click: SET_FILTER('notDone')">not done</a>
+      </p>
+      {{remaining}} {{remaining | pluralize 'item'}} left
+      <button v-on="click: CLEAR_DONE_TODOS">Clear Done</button>
+    </div>
   </div>
 </template>
