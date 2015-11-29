d74fd8327940867bf4870ec6cbfe9d1d7521a09a
Evan You <yyx990803@gmail.com>
2015-11-29 01:50:28 -0500

    small middleware tweak

--- examples/todomvc/vuex/middlewares.js
+++ examples/todomvc/vuex/middlewares.js
@@ -1,7 +1,7 @@
 import { STORAGE_KEY } from './index'
 
 export default [{
-  after: function (action, state) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(state.todos))
+  after: function (mutation, { todos }) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }]
