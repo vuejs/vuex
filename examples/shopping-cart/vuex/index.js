edbf9b1be19547c731e8e986152eacb7dfaef677
Evan You <yyx990803@gmail.com>
2015-11-30 12:02:28 -0500

    rename stores to modules

--- examples/shopping-cart/vuex/index.js
+++ examples/shopping-cart/vuex/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from '../../../src'
 import logger from '../../../src/middlewares/logger'
 import * as actions from './actions'
-import { cartInitialState, cartMutations } from './stores/cart'
-import { productsInitialState, productsMutations } from './stores/products'
+import { cartInitialState, cartMutations } from './modules/cart'
+import { productsInitialState, productsMutations } from './modules/products'
 
 Vue.use(Vuex)
 Vue.config.debug = true
