2203bb5effc2009830d641fa20948ed39a426b1c
Evan You <yyx990803@gmail.com>
2015-12-03 22:35:22 -0500

    doc deploy script

---
+++ docs/deploy.sh
@@ -0,0 +1,9 @@
+rm -rf _book
+gitbook build
+cp assets/circle.yml _book/circle.yml
+cp assets/CNAME _book/CNAME
+cd _book
+git init
+git add -A
+git commit -m 'update book'
+git push -f git@github.com:vuejs/vuex.git master:gh-pages
