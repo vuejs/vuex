5fe5305b891ba6e850c873867da597335fe001f2
Evan You <yyx990803@gmail.com>
2016-02-03 16:53:56 -0500

    support store injection

--- src/index.js
+++ src/index.js
@@ -223,6 +223,16 @@ export { createLogger }
 // export install function
 export function install (_Vue) {
   Vue = _Vue
+  const _init = Vue.prototype._init
+  Vue.prototype._init = function (options) {
+    options = options || {}
+    if (options.store) {
+      this.$store = options.store
+    } else if (options.parent && options.parent.$store) {
+      this.$store = options.parent.$store
+    }
+    _init.call(this, options)
+  }
 }
 
 // also export the default
