3c6d085ab82bc2f6f0b533b4808e5f1d35f2c7ce
Evan You <yyx990803@gmail.com>
2016-03-01 18:16:56 -0500

    dist build

---
+++ build/build.js
@@ -0,0 +1,70 @@
+process.env.BABEL_ENV = 'production'
+
+var fs = require('fs')
+var zlib = require('zlib')
+var rollup = require('rollup')
+var uglify = require('uglify-js')
+var babel = require('rollup-plugin-babel')
+var version = process.env.VERSION || require('../package.json').version
+
+var banner =
+  '/*!\n' +
+  ' * Vuex v' + version + '\n' +
+  ' * (c) ' + new Date().getFullYear() + ' Evan You\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+rollup.rollup({
+  entry: 'src/index.js',
+  plugins: [babel()]
+})
+.then(function (bundle) {
+  return write('dist/vuex.js', bundle.generate({
+    format: 'umd',
+    banner: banner,
+    moduleName: 'Vuex'
+  }).code)
+})
+.then(function () {
+  // Standalone Production Build
+  return rollup.rollup({
+    entry: 'src/index.js',
+    plugins: [babel()]
+  })
+})
+.then(function (bundle) {
+  var code = bundle.generate({
+    format: 'umd',
+    moduleName: 'Vuex'
+  }).code
+  var minified = banner + '\n' + uglify.minify(code, {
+    fromString: true,
+    output: {
+      ascii_only: true
+    }
+  }).code
+  return write('dist/vuex.min.js', minified)
+})
+.catch(logError)
+
+function write (dest, code) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(dest, code, function (err) {
+      if (err) return reject(err)
+      console.log(blue(dest) + ' ' + getSize(code))
+      resolve()
+    })
+  })
+}
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
+}
+
+function logError (e) {
+  console.log(e)
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
