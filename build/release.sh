d5fdaeadca2691fcd56d54fb79990791413bc749
Evan You <yyx990803@gmail.com>
2016-03-08 05:13:54 -0500

    release script

--- build/release.sh
+++ build/release.sh
@@ -6,21 +6,21 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Releasing $VERSION ..."
+  echo "Releasing $VERSION ..."
 
-    # run tests
-    npm test 2>/dev/null
+  # run tests
+  npm test 2>/dev/null
 
-    # build
-    VERSION=$VERSION npm run build
+  # build
+  VERSION=$VERSION npm run build
 
-    # # commit
-    git add -A
-    git commit -m "[build] $VERSION"
-    npm version $VERSION --message "[release] $VERSION"
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
 
-    # # publish
-    git push origin refs/tags/v$VERSION
-    git push
-    npm publish
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+  npm publish
 fi
