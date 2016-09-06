d719e8c5b9264f2a5dc55a7e8cf95884ddcc0f23
ktsn <ktsn55@gmail.com>
2016-09-06 22:31:44 +0900

    update d.ts tests for 2.0

---
+++ types/test/helpers.ts
@@ -0,0 +1,47 @@
+import * as Vue from "vue";
+
+import {
+  mapState,
+  mapGetters,
+  mapActions,
+  mapMutations
+} from "../index";
+
+new Vue({
+  computed: Object.assign({},
+    mapState(["a"]),
+    mapState({
+      b: "b"
+    }),
+    mapState({
+      c: (state: any, getters: any) => state.c + getters.c
+    }),
+
+    mapGetters(["d"]),
+    mapGetters({
+      e: "e"
+    }),
+
+    {
+      otherComputed () {
+        return "f";
+      }
+    }
+  ),
+
+  methods: Object.assign({},
+    mapActions(["g"]),
+    mapActions({
+      h: "h"
+    }),
+
+    mapMutations(["i"]),
+    mapMutations({
+      j: "j"
+    }),
+
+    {
+      otherMethod () {}
+    }
+  )
+});
