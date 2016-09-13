1ed7216230d1d21283433114aee7393297406734
ktsn <ktsn55@gmail.com>
2016-09-13 23:16:15 +0900

    use state transformer in logger plugin

--- src/plugins/logger.js
+++ src/plugins/logger.js
@@ -28,9 +28,9 @@ export default function createLogger ({
         console.log(message)
       }
 
-      console.log('%c prev state', 'color: #9E9E9E; font-weight: bold', prevState)
+      console.log('%c prev state', 'color: #9E9E9E; font-weight: bold', transformer(prevState))
       console.log('%c mutation', 'color: #03A9F4; font-weight: bold', formattedMutation)
-      console.log('%c next state', 'color: #4CAF50; font-weight: bold', nextState)
+      console.log('%c next state', 'color: #4CAF50; font-weight: bold', transformer(nextState))
 
       try {
         console.groupEnd()
