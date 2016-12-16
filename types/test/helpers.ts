8be59b4e5284bf2e6e3039c0391d38bf0368fbd9
Evan You <yyx990803@gmail.com>
2016-12-16 18:12:02 -0500

    update types for new helper signatures

--- types/test/helpers.ts
+++ types/test/helpers.ts
@@ -10,15 +10,26 @@ import {
 new Vue({
   computed: Object.assign({},
     mapState(["a"]),
+    mapState('foo', ["a"]),
     mapState({
+      b: "b"
+    }),
+    mapState('foo', {
       b: "b"
     }),
     mapState({
       c: (state: any, getters: any) => state.c + getters.c
     }),
+    mapState('foo', {
+      c: (state: any, getters: any) => state.c + getters.c
+    }),
 
     mapGetters(["d"]),
+    mapGetters('foo', ["d"]),
     mapGetters({
+      e: "e"
+    }),
+    mapGetters('foo', {
       e: "e"
     }),
 
@@ -34,9 +45,17 @@ new Vue({
     mapActions({
       h: "h"
     }),
+    mapActions('foo', ["g"]),
+    mapActions('foo', {
+      h: "h"
+    }),
 
     mapMutations(["i"]),
     mapMutations({
+      j: "j"
+    }),
+    mapMutations('foo', ["i"]),
+    mapMutations('foo', {
       j: "j"
     }),
 
