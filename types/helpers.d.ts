1b4e2de0804e58f2d73342448ba2db3f1dc64d6b
ktsn <ktsn55@gmail.com>
2017-08-16 13:58:45 +0900

    fix(typings): add missing createNamespacedHelpers typings

--- types/helpers.d.ts
+++ types/helpers.d.ts
@@ -1,32 +1,52 @@
 import Vue = require("vue");
 
 type Dictionary<T> = { [key: string]: T };
-
-export function mapState (map: string[]): Dictionary<() => any>;
-export function mapState (namespace: string, map: string[]): Dictionary<() => any>;
-export function mapState (map: Dictionary<string>): Dictionary<() => any>;
-export function mapState (namespace: string, map: Dictionary<string>): Dictionary<() => any>;
-export function mapState <S>(
-  map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
-): Dictionary<() => any>;
-export function mapState <S>(
-  namespace: string,
-  map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
-): Dictionary<() => any>;
-
+type Computed = () => any;
 type MutationMethod = (...args: any[]) => void;
-export function mapMutations (map: string[]): Dictionary<MutationMethod>;
-export function mapMutations (namespace: string, map: string[]): Dictionary<MutationMethod>;
-export function mapMutations (map: Dictionary<string>): Dictionary<MutationMethod>;
-export function mapMutations (namespace: string, map: Dictionary<string>): Dictionary<MutationMethod>;
+type ActionMethod = (...args: any[]) => Promise<any>;
 
-export function mapGetters (map: string[]): Dictionary<() => any>;
-export function mapGetters (namespace: string, map: string[]): Dictionary<() => any>;
-export function mapGetters (map: Dictionary<string>): Dictionary<() => any>;
-export function mapGetters (namespace: string, map: Dictionary<string>): Dictionary<() => any>;
+type Mapper<R> = {
+  (map: string[]): Dictionary<R>;
+  (map: Dictionary<string>): Dictionary<R>;
+};
 
-type ActionMethod = (...args: any[]) => Promise<any[]>;
-export function mapActions (map: string[]): Dictionary<ActionMethod>;
-export function mapActions (namespace: string, map: string[]): Dictionary<ActionMethod>;
-export function mapActions (map: Dictionary<string>): Dictionary<ActionMethod>;
-export function mapActions (namespace: string, map: Dictionary<string>): Dictionary<ActionMethod>;
+type MapperWithNamespace<R> = {
+  (namespace: string, map: string[]): Dictionary<R>;
+  (namespace: string, map: Dictionary<string>): Dictionary<R>;
+};
+
+type MapperForState = {
+  <S>(
+    map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
+  ): Dictionary<Computed>;
+};
+
+type MapperForStateWithNamespace = {
+  <S>(
+    namespace: string,
+    map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
+  ): Dictionary<Computed>;
+};
+
+interface NamespacedMappers {
+  mapState: Mapper<Computed> & MapperForState;
+  mapMutations: Mapper<MutationMethod>;
+  mapGetters: Mapper<Computed>;
+  mapActions: Mapper<ActionMethod>;
+}
+
+export declare const mapState: Mapper<Computed>
+  & MapperWithNamespace<Computed>
+  & MapperForState
+  & MapperForStateWithNamespace;
+
+export declare const mapMutations: Mapper<MutationMethod>
+  & MapperWithNamespace<MutationMethod>;
+
+export declare const mapGetters: Mapper<Computed>
+  & MapperWithNamespace<Computed>;
+
+export declare const mapActions: Mapper<ActionMethod>
+  & MapperWithNamespace<ActionMethod>;
+
+export declare function createNamespacedHelpers(namespace: string): NamespacedMappers;
