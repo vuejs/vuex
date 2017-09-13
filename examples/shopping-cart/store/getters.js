c4fcfadf818ec125ee36e99fdc012bf1f1a330f2
Connor Leech <connorleech@gmail.com>
2017-09-13 11:53:58 -0700

    state.cart is undefined. update to state.added

--- examples/shopping-cart/store/getters.js
+++ examples/shopping-cart/store/getters.js
@@ -1,5 +1,5 @@
 export const cartProducts = state => {
-  return state.cart.added.map(({ id, quantity }) => {
+  return state.added.map(({ id, quantity }) => {
     const product = state.products.all.find(p => p.id === id)
     return {
       title: product.title,
