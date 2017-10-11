0e9756b93c5de8e03286d93f0b50af5f8dfd3bac
Nikita Tolkachev <n7olkachev@gmail.com>
2017-10-11 21:34:39 +0300

    fix: initialize root state as an empty object if state function returns no value (#927)
    * assert state to be an object
    
    Just a moment ago had hard time debugging this code:
    ```js
    const state = () => {}
    ...
    new Vuex.Store({
        state,
        modules,
        ...
    })
    ```
    If state is `undefined` and you use modules, Vuex can't create store. This small change will make it easier to find this error.
    
    * fix styling
    
    * provide default value for state as a function

--- src/store.js
+++ src/store.js
@@ -29,7 +29,7 @@ export class Store {
       state = {}
     } = options
     if (typeof state === 'function') {
-      state = state()
+      state = state() || {}
     }
 
     // store internal state
