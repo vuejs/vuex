0768c802b0634ff44f3d8f922b718511398efb34
ktsn <ktsn55@gmail.com>
2017-11-06 12:10:56 +0900

    feat: improve helper types to utilize Vue 2.5 types

--- types/helpers.d.ts
+++ types/helpers.d.ts
@@ -1,68 +1,76 @@
 import Vue from 'vue';
 import { Dispatch, Commit } from './index';
 
-type Dictionary<T> = { [key: string]: T };
-type Computed = () => any;
-type MutationMethod = (...args: any[]) => void;
-type ActionMethod = (...args: any[]) => Promise<any>;
+type Computed<R> = () => R;
+type MutationMethod<P> = (payload: P) => void;
+type ActionMethod<P> = (payload: P) => Promise<any>;
 
-interface Mapper<R> {
-  (map: string[]): Dictionary<R>;
-  (map: Dictionary<string>): Dictionary<R>;
+type StateAccessor<T, State, Getters> = {
+  [K in keyof T]: (this: Vue, state: State, getters: Getters) => T[K];
+} & {
+  [key: string]: (this: Vue, state: State, getters: Getters) => any;
 }
 
-interface MapperWithNamespace<R> {
-  (namespace: string, map: string[]): Dictionary<R>;
-  (namespace: string, map: Dictionary<string>): Dictionary<R>;
+interface BaseType { [key: string]: any }
+
+type BaseMethodMap<F> = { [key: string]: (this: Vue, fn: F, ...args: any[]) => any }
+
+interface MapGetters<Getters> {
+  <T = Getters, Key extends keyof T = keyof T>(map: Key[]): { [K in Key]: Computed<T[K]> };
+  <T = Getters, Map extends Record<string, keyof T> = Record<string, keyof T>>(map: Map): { [K in keyof Map]: Computed<T[Map[K]]> };
 }
 
-interface FunctionMapper<F, R> {
-  (map: Dictionary<(this: typeof Vue, fn: F, ...args: any[]) => any>): Dictionary<R>;
+interface RootMapGetters<Getters> extends MapGetters<Getters> {
+  <T = Getters, Key extends keyof T = keyof T>(namespace: string, map: Key[]): { [K in Key]: Computed<T[K]> };
+  <T = Getters, Map extends Record<string, keyof T> = Record<string, keyof T>>(namespace: string, map: Map): { [K in keyof Map]: Computed<T[Map[K]]> };
 }
 
-interface FunctionMapperWithNamespace<F, R> {
-  (
-    namespace: string,
-    map: Dictionary<(this: typeof Vue, fn: F, ...args: any[]) => any>
-  ): Dictionary<R>;
+interface MapState<State> extends MapGetters<State> {
+  <State, Getters, T>(map: StateAccessor<T, State, Getters>): { [K in keyof T]: Computed<T[K]> };
 }
 
-interface MapperForState {
-  <S>(
-    map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
-  ): Dictionary<Computed>;
+type CombinedMapState<State> = MapState<State> & RootMapGetters<State>
+interface RootMapState<State> extends CombinedMapState<State> {
+  <State, Getters, T>(namespace: string, map: StateAccessor<T, State, Getters>): { [K in keyof T]: Computed<T[K]> };
 }
 
-interface MapperForStateWithNamespace {
-  <S>(
-    namespace: string,
-    map: Dictionary<(this: typeof Vue, state: S, getters: any) => any>
-  ): Dictionary<Computed>;
+interface MapMutations<Mutations> {
+  <T = Mutations, Key extends keyof T = keyof T>(map: Key[]): { [K in Key]: MutationMethod<T[K]> };
+  <T = Mutations, Map extends Record<string, keyof T> = Record<string, keyof T>>(map: Map): { [K in keyof Map]: MutationMethod<T[Map[K]]> };
+  <T extends BaseMethodMap<Commit>>(map: T): { [K in keyof T]: Function };
 }
 
-interface NamespacedMappers {
-  mapState: Mapper<Computed> & MapperForState;
-  mapMutations: Mapper<MutationMethod> & FunctionMapper<Commit, MutationMethod>;
-  mapGetters: Mapper<Computed>;
-  mapActions: Mapper<ActionMethod> & FunctionMapper<Dispatch, ActionMethod>;
+interface RootMapMutations<Mutations> extends MapMutations<Mutations> {
+  <T = Mutations, Key extends keyof T = keyof T>(namespace: string, map: Key[]): { [K in Key]: MutationMethod<T[K]> };
+  <T = Mutations, Map extends Record<string, keyof T> = Record<string, keyof T>>(namespace: string, map: Map): { [K in keyof Map]: MutationMethod<T[Map[K]]> };
+  <T extends BaseMethodMap<Commit>>(namespace: string, map: T): { [K in keyof T]: Function };
 }
 
-export declare const mapState: Mapper<Computed>
-  & MapperWithNamespace<Computed>
-  & MapperForState
-  & MapperForStateWithNamespace;
+interface MapActions<Actions> {
+  <T = Actions, Key extends keyof T = keyof T>(map: Key[]): { [K in Key]: ActionMethod<T[K]> };
+  <T = Actions, Map extends Record<string, keyof T> = Record<string, keyof T>>(map: Map): { [K in keyof Map]: ActionMethod<T[Map[K]]> };
+  <T extends BaseMethodMap<Dispatch>>(map: T): { [K in keyof T]: Function };
+}
 
-export declare const mapMutations: Mapper<MutationMethod>
-  & MapperWithNamespace<MutationMethod>
-  & FunctionMapper<Commit, MutationMethod>
-  & FunctionMapperWithNamespace<Commit, MutationMethod>;
+interface RootMapActions<Actions> extends MapActions<Actions> {
+  <T = Actions, Key extends keyof T = keyof T>(namespace: string, map: Key[]): { [K in Key]: ActionMethod<T[K]> };
+  <T = Actions, Map extends Record<string, keyof T> = Record<string, keyof T>>(namespace: string, map: Map): { [K in keyof Map]: ActionMethod<T[Map[K]]> };
+  <T extends BaseMethodMap<Dispatch>>(namespace: string, map: T): { [K in keyof T]: Function };
+}
 
-export declare const mapGetters: Mapper<Computed>
-  & MapperWithNamespace<Computed>;
+interface NamespacedMappers<State, Getters, Mutations, Actions> {
+  mapState: MapState<State>;
+  mapMutations: MapMutations<Getters>;
+  mapGetters: MapGetters<Mutations>;
+  mapActions: MapActions<Actions>;
+}
 
-export declare const mapActions: Mapper<ActionMethod>
-  & MapperWithNamespace<ActionMethod>
-  & FunctionMapper<Dispatch, ActionMethod>
-  & FunctionMapperWithNamespace<Dispatch, ActionMethod>;
+export declare const mapState: RootMapState<BaseType>;
 
-export declare function createNamespacedHelpers(namespace: string): NamespacedMappers;
+export declare const mapMutations: RootMapMutations<BaseType>;
+
+export declare const mapGetters: RootMapGetters<BaseType>;
+
+export declare const mapActions: RootMapActions<BaseType>;
+
+export declare function createNamespacedHelpers<State = BaseType, Getters = BaseType, Mutations = BaseType, Actions = BaseType>(namespace: string): NamespacedMappers<BaseType, BaseType, BaseType, BaseType>;
