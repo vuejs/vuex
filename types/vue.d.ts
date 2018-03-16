1a9f56a1490a796a7f0d24b1819dfc90adfedfc3
HerringtonDarkholme <HerringtonDarkholme@users.noreply.github.com>
2018-03-16 11:23:13 +0800

    feat(type): add RootState for store type customization
    fix #994

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -7,12 +7,20 @@ import { Store } from "./index";
 
 declare module "vue/types/options" {
   interface ComponentOptions<V extends Vue> {
-    store?: Store<any>;
+    store?: Store<PermissiveAny<RootState>>;
   }
 }
 
 declare module "vue/types/vue" {
   interface Vue {
-    $store: Store<any>;
+    $store: Store<PermissiveAny<RootState>>;
   }
 }
+
+export type PermissiveAny<T> = {} extends T ? any : T
+
+// stub for user augmentation
+export interface RootState {}
+export interface RootActions {}
+export interface RootMutations {}
+export interface RootGetters {}
