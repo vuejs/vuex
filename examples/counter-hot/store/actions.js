b8c0f1f045fe124ef55fe13abe6ed4cc77b3d392
Alex Kyriakidis <hootlex@icloud.com>
2018-04-10 20:20:18 +0200

    Make mutations responsible for updating one piece

--- examples/counter-hot/store/actions.js
+++ examples/counter-hot/store/actions.js
@@ -1,14 +1,22 @@
-export const increment = ({ commit }) => commit('increment')
-export const decrement = ({ commit }) => commit('decrement')
+export const increment = ({ commit }) => {
+  commit('increment')
+  commit('pushToHistory', 'increment')
+}
+export const decrement = ({ commit }) => {
+  commit('decrement')
+  commit('pushToHistory', 'decrement')
+}
 
 export const incrementIfOdd = ({ commit, state }) => {
   if ((state.count + 1) % 2 === 0) {
     commit('increment')
+    commit('pushToHistory', 'increment')
   }
 }
 
 export const incrementAsync = ({ commit }) => {
   setTimeout(() => {
     commit('increment')
+    commit('pushToHistory', 'decrement')
   }, 1000)
 }
--- examples/counter-hot/store/mutations.js
+++ examples/counter-hot/store/mutations.js
@@ -1,9 +1,11 @@
 export const increment = state => {
   state.count++
-  state.history.push('increment')
 }
 
 export const decrement = state => {
   state.count--
-  state.history.push('decrement')
+}
+
+export const pushToHistory = (state, item) => {
+  state.history.push(item)
 }
