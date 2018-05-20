1af856e833833e1843dca0b7295c6513ea14dbf1
Jinjiang <zhaojinjiang@me.com>
2018-05-20 23:41:39 +0800

    [docs] gitbook -> vuepress

--- docs/deploy.sh
+++
@@ -1,9 +0,0 @@
-rm -rf _book
-gitbook build
-cp assets/circle.yml _book/circle.yml
-cp assets/CNAME _book/CNAME
-cd _book
-git init
-git add -A
-git commit -m 'update book'
-git push -f git@github.com:vuejs/vuex.git master:gh-pages
