ecadebcb063ccda5f3bc4e1e112e5071fc9bd4b9
Alex Kyriakidis <hootlex@icloud.com>
2018-06-17 17:48:44 +0200

    update history from mutations

--- examples/counter-hot/store/actions.js
+++ examples/counter-hot/store/actions.js
@@ -4,19 +4,16 @@ export const increment = ({ commit }) => {
 }
 export const decrement = ({ commit }) => {
   commit('decrement')
-  commit('pushToHistory', 'decrement')
 }
 
 export const incrementIfOdd = ({ commit, state }) => {
   if ((state.count + 1) % 2 === 0) {
     commit('increment')
-    commit('pushToHistory', 'increment')
   }
 }
 
 export const incrementAsync = ({ commit }) => {
   setTimeout(() => {
     commit('increment')
-    commit('pushToHistory', 'decrement')
   }, 1000)
 }
--- examples/counter-hot/store/mutations.js
+++ examples/counter-hot/store/mutations.js
@@ -1,11 +1,9 @@
 export const increment = state => {
   state.count++
+  state.history.push('increment')
 }
 
 export const decrement = state => {
   state.count--
-}
-
-export const pushToHistory = (state, item) => {
-  state.history.push(item)
+  state.history.push('decrement')
 }
