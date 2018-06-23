63ce98661c9a8692f72483c0fa8a6c1ba84d178a
zjfeng1223 <zjfeng1223@gmail.com>
2018-06-24 00:10:42 +0800

    modify shopping-cart store cart.js

--- examples/shopping-cart/store/modules/cart.js
+++ examples/shopping-cart/store/modules/cart.js
@@ -69,8 +69,8 @@ const mutations = {
     })
   },
 
-  incrementItemQuantity (state, { id }) {
-    const cartItem = state.items.find(item => item.id === id)
+  incrementItemQuantity (state, cartItem) {
+    // const cartItem = state.items.find(item => item.id === id)
     cartItem.quantity++
   },
 
