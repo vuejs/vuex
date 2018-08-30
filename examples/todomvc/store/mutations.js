de0448202bfe1a30549551a7ae1f6f036f0e6fc0
luominmin <102625110@hdjt.hdad.local>
2018-08-30 19:49:18 +0800

    fix bug: example todomvc, delete the anotation

--- examples/todomvc/store/mutations.js
+++ examples/todomvc/store/mutations.js
@@ -11,12 +11,7 @@ export const mutations = {
   },
 
   removeTodo (state, todo) {
-    // when edit some one todoItem, clear the todoItem, then press keyboard enter 
-    // because execute @keyup.enter="doneEdit" and @keyup.esc="cancelEdit" (in page TodoItem.vue)
-    // so here will execute twice, so will delete two todoItem
-    // so i suggest change here to 
     state.todos = state.todos.filter(item => item.id !== todo.id)
-    // state.todos.splice(state.todos.indexOf(todo), 1)
   },
 
   editTodo (state, { todo, text = todo.text, done = todo.done }) {
