8335168158e5844c0aee5409c1037590268372ee
lishihong <546537081@qq.com>
2019-01-10 17:38:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Store, install } from './store'
 import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
 export default {
-  Store,
+  Storeaaa,
   install,
   version: '__VERSION__',
   mapState,
