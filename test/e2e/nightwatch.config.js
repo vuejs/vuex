ee5a9ec0076a73f1b665c815495e7369b9c14435
Kia Ishii <kia.king.08@gmail.com>
2019-11-19 16:19:40 +0900

    Remove PhantonJS from E2E test, and use Chrom Headless Webdriver

--- test/e2e/nightwatch.config.js
+++ test/e2e/nightwatch.config.js
@@ -5,20 +5,12 @@ module.exports = {
   'custom_commands_path': ['node_modules/nightwatch-helpers/commands'],
   'custom_assertions_path': ['node_modules/nightwatch-helpers/assertions'],
 
-  'selenium': {
-    'start_process': true,
-    'server_path': require('selenium-server').path,
-    'host': '127.0.0.1',
-    'port': 4444,
-    'cli_args': {
-      'webdriver.chrome.driver': require('chromedriver').path
-    }
+  'webdriver': {
+    'start_process': true
   },
 
   'test_settings': {
     'default': {
-      'selenium_port': 4444,
-      'selenium_host': 'localhost',
       'silent': true,
       'screenshots': {
         'enabled': true,
@@ -29,19 +21,18 @@ module.exports = {
     },
 
     'chrome': {
+      'webdriver': {
+        'port': 9515,
+        'server_path': require('chromedriver').path
+      },
+
       'desiredCapabilities': {
         'browserName': 'chrome',
         'javascriptEnabled': true,
-        'acceptSslCerts': true
-      }
-    },
-
-    'phantomjs': {
-      'desiredCapabilities': {
-        'browserName': 'phantomjs',
-        'javascriptEnabled': true,
         'acceptSslCerts': true,
-        'phantomjs.binary.path': require('phantomjs-prebuilt').path
+        'chromeOptions': {
+          'args': ['--headless']
+        }
       }
     }
   }
