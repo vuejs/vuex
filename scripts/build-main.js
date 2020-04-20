bfcb86d51ad788c6b10e3488fa6da8de28a9ad75
Kia Ishii <kia.king.08@gmail.com>
2020-04-20 18:16:54 +0900

    build: add jsdelivr support and rename `esm` to `esm-browser`

--- scripts/build-main.js
+++ scripts/build-main.js
@@ -1,8 +1,8 @@
 const { run } = require('./build')
 
 const files = [
-  'dist/vuex.esm.js',
-  'dist/vuex.esm.prod.js',
+  'dist/vuex.esm-browser.js',
+  'dist/vuex.esm-browser.prod.js',
   'dist/vuex.esm-bundler.js',
   'dist/vuex.global.js',
   'dist/vuex.global.prod.js',
