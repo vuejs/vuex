30bbfba46db5fbf5641ee4f5b75d2089041c8779
Eduard Aksamitov <e@edaks.co>
2020-08-05 15:32:07 +0300

    fix(types): typo in generic

--- types/index.d.ts
+++ types/index.d.ts
@@ -45,7 +45,7 @@ export declare class Store<S> {
 
 export function createStore<S>(options: StoreOptions<S>): Store<S>;
 
-export function useStore<S = any>(injectKey?: InjectionKey<Store<any>> | string): Store<S>;
+export function useStore<S = any>(injectKey?: InjectionKey<Store<S>> | string): Store<S>;
 
 export interface Dispatch {
   (type: string, payload?: any, options?: DispatchOptions): Promise<any>;
