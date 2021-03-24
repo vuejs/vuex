8d9ad88563d614376097bb3e4077cd9f021e468a
Eduardo San Martin Morote <posva13@gmail.com>
2021-03-24 15:43:50 +0100

    feat(devtools): add basic state+getters inspection and timeline

--- src/plugins/devtool.js
+++ src/plugins/devtool.js
@@ -1,14 +1,174 @@
 import { setupDevtoolsPlugin } from '@vue/devtools-api'
 
+const LABEL_VUEX_BINDINGS = 'vuex bindings'
+/** @type {boolean | undefined} */
+let isAlreadyInstalled
+
+const MUTATIONS_LAYER_ID = 'vuex:mutations'
+const INSPECTOR_ID = 'vuex'
+
 export function addDevtools (app, store) {
-  setupDevtoolsPlugin({
-    id: 'vuex',
-    app,
-    label: 'Vuex',
-    homepage: 'https://next.vuex.vuejs.org/',
-    logo: 'https://vuejs.org/images/icons/favicon-96x96.png',
-    packageName: 'vuex'
-  }, api => {
-    // @TODO
-  })
+  setupDevtoolsPlugin(
+    {
+      id: 'org.vuejs.vuex',
+      app,
+      label: 'Vuex',
+      homepage: 'https://next.vuex.vuejs.org/',
+      logo: 'https://vuejs.org/images/icons/favicon-96x96.png',
+      packageName: 'vuex',
+      componentStateTypes: [LABEL_VUEX_BINDINGS]
+    },
+    (api) => {
+      if (!isAlreadyInstalled) {
+        api.addTimelineLayer({
+          id: MUTATIONS_LAYER_ID,
+          label: 'Vuex',
+          color: COLOR_LIME_500
+        })
+
+        api.addInspector({
+          id: INSPECTOR_ID,
+          label: 'Vuex',
+          icon: 'storage',
+          treeFilterPlaceholder: 'Filter stores...'
+        })
+
+        isAlreadyInstalled = true
+
+        api.on.getInspectorTree((payload) => {
+          if (payload.app === app && payload.inspectorId === INSPECTOR_ID) {
+          // TODO: filter with payload
+            payload.rootNodes = [
+              formatStoreForInspectorTree(store._modules.root, '')
+            ]
+          }
+        })
+
+        api.on.getInspectorState((payload) => {
+          if (payload.app === app && payload.inspectorId === INSPECTOR_ID) {
+            const modulePath = payload.nodeId
+            payload.state = formatStoreForInspectorState(
+              getStoreModule(store._modules, modulePath),
+              store._makeLocalGettersCache,
+              modulePath
+            )
+          }
+        })
+      } else {
+        api.notifyComponentUpdate()
+        api.sendInspectorTree(INSPECTOR_ID)
+        api.sendInspectorState(INSPECTOR_ID)
+      }
+
+      store.subscribe((mutation, state) => {
+        const data = { state }
+
+        if (mutation.payload) {
+          data.payload = mutation.payload
+        }
+
+        api.notifyComponentUpdate()
+        api.sendInspectorTree(INSPECTOR_ID)
+        api.sendInspectorState(INSPECTOR_ID)
+
+        api.addTimelineEvent({
+          layerId: MUTATIONS_LAYER_ID,
+          event: {
+            time: Date.now(),
+            title: mutation.type,
+            data
+          }
+        })
+      })
+    }
+  )
+}
+
+/**
+ * Extracted from tailwind palette
+ */
+const COLOR_PINK_500 = 0xec4899
+const COLOR_BLUE_600 = 0x2563eb
+const COLOR_LIME_500 = 0x84cc16
+const COLOR_CYAN_400 = 0x22d3ee
+const COLOR_ORANGE_400 = 0xfb923c
+// const COLOR_GRAY_100 = 0xf4f4f5
+const COLOR_DARK = 0x666666
+const COLOR_WHITE = 0xffffff
+
+const TAG_NAMESPACED = {
+  label: 'namespaced',
+  textColor: COLOR_WHITE,
+  backgroundColor: COLOR_DARK
+}
+
+/**
+ *
+ * @param {string} path
+ */
+function extractNameFromPath (path) {
+  return path && path !== 'root' ? path.split('/').slice(-2, -1)[0] : 'Root'
+}
+
+/**
+ *
+ * @param {*} module
+ * @return {import('@vue/devtools-api').CustomInspectorNode}
+ */
+function formatStoreForInspectorTree (module, path) {
+  return {
+    id: path || 'root',
+    // all modules end with a `/`, we want the last segment only
+    // cart/ -> cart
+    // nested/cart/ -> cart
+    label: extractNameFromPath(path),
+    tags: module.namespaced ? [TAG_NAMESPACED] : [],
+    children: Object.keys(module._children).map((moduleName) =>
+      formatStoreForInspectorTree(
+        module._children[moduleName],
+        path + moduleName + '/'
+      )
+    )
+  }
+}
+
+/**
+ *
+ * @param {*} module
+ * @return {import('@vue/devtools-api').CustomInspectorState}
+ */
+function formatStoreForInspectorState (module, getters, path) {
+  getters = path === 'root' ? getters : getters[path]
+  const gettersKeys = Object.keys(getters)
+  const storeState = {
+    state: Object.keys(module.state).map((key) => ({
+      key,
+      editable: true,
+      value: module.state[key]
+    }))
+  }
+
+  if (gettersKeys.length) {
+    storeState.getters = gettersKeys.map((key) => ({
+      key: key.endsWith('/') ? extractNameFromPath(key) : key,
+      editable: false,
+      value: getters[key]
+    }))
+  }
+
+  return storeState
+}
+
+function getStoreModule (moduleMap, path) {
+  const names = path.split('/').filter((n) => n)
+  return names.reduce(
+    (module, moduleName, i) => {
+      const child = module[moduleName]
+      if (!child) {
+        throw new Error(`Missing module "${moduleName}" for path "${path}".`)
+      }
+      return i === names.length - 1 ? child : child._children
+    },
+    path === 'root' ? moduleMap : moduleMap.root._children
+  )
 }
