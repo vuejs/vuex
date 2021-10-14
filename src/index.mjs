13c760bb09c9c593c89697d983f54b45c196f549
Daniel Roe <daniel@roe.dev>
2021-10-14 08:57:16 +0100

    fix: revert to previous export syntax

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,26 @@
-export * from '../dist/vuex.esm.js'
+import Vuex from '../dist/vuex.esm.js'
+
+const {
+  Store,
+  install,
+  version,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+} = Vuex
+
+export {
+  Vuex as default,
+  Store,
+  install,
+  version,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+}
