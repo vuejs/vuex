0f80c193cef216c454923fd9060e9c3bdf2c7111
zhangenming <282126346@qq.com>
2021-11-10 18:49:59 +0800

    chore: typo fix in the comment (#2086)

--- src/helpers.js
+++ src/helpers.js
@@ -140,7 +140,7 @@ export const createNamespacedHelpers = (namespace) => ({
  * normalizeMap([1, 2, 3]) => [ { key: 1, val: 1 }, { key: 2, val: 2 }, { key: 3, val: 3 } ]
  * normalizeMap({a: 1, b: 2, c: 3}) => [ { key: 'a', val: 1 }, { key: 'b', val: 2 }, { key: 'c', val: 3 } ]
  * @param {Array|Object} map
- * @return {Object}
+ * @return {Array}
  */
 function normalizeMap (map) {
   if (!isValidMap(map)) {
